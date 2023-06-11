b3146c41748d666e4c5fe70876345579d54a931d
Mohamed Yusuf <study@MohamedYusufs-MacBook-Pro.local>
2023-06-11 18:04:18 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-function addingEventListener() {
+function addingEventListener(){
+	const input = document.getElementById('button');
+	
+	function ev() {
+	  console.log('I was clicked!');
+	}
+
+	input.addEventListener('click', ev);
+
 }
+
+
+	
+
